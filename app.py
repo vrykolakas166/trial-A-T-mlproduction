91d07e7bea0ebb718fa1e8a772a9a0df39fdc93f
Phúc Phạm <hhtb.bdh@gmail.com>
2024-12-09 13:25:43 +0700

    clean

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import flask
+# import pickle
+# import numpy as np
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         preg = int(request.form['pregnancies'])
+#         glucose = int(request.form['glucose'])
+#         bp = int(request.form['bloodpressure'])
+#         st = int(request.form['skinthickness'])
+#         insulin = int(request.form['insulin'])
+#         bmi = float(request.form['bmi'])
+#         dpf = float(request.form['dpf'])
+#         age = int(request.form['age'])
+        
+#         data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+#         filename = 'diabetes-prediction-rfc-model.pkl'
+#         with open(filename, "rb") as file_obj:
+#             classifier = pickle.load(file_obj)
+#         my_prediction = classifier.predict(data)
+        
+#         return render_template('result.html', prediction=my_prediction)
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0",debug=True)
